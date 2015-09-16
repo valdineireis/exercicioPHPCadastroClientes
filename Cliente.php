080ad84dd11b5fa7210bb57ee30f5c116238b7b0
Valdinei Reis <valdineimtz@gmail.com>
2015-09-16 00:03:48 -0300

    Objeto Cliente criado

---
+++ Cliente.php
@@ -0,0 +1,56 @@
+<?php
+
+class Cliente
+{
+    private $nome;
+    private $cpf;
+    private $endereco;
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * @param mixed $cpf
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+
+    /**
+     * @param mixed $endereco
+     */
+    public function setEndereco($endereco)
+    {
+        $this->endereco = $endereco;
+    }
+}
