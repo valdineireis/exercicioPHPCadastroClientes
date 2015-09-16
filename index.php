ab22d4df9cc4fdfb19f36d87cd3a467905e6deda
Valdinei Reis <valdineimtz@gmail.com>
2015-09-16 00:49:09 -0300

    Melhorando a exibição com Bootstrap

--- index.php
+++ index.php
@@ -14,18 +14,48 @@ $cliente = new ClienteController();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Exercício básico de PHP demonstrando um simples cadastro de clientes em memória">
     <meta name="author" content="Valdinei Reis">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/footer-navbar.css">
+
 </head>
 <body>
-    <table>
-        <thead>
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Exercício PHP</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Clientes</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <!-- Begin page content -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Cadastro de Clientes</h1>
+        </div>
+
+        <table class="table table-striped table-condensed">
+            <thead>
             <tr>
                 <th>Nome</th>
                 <th>CPF</th>
                 <th>Ednereço</th>
             </tr>
-        </thead>
-        <tbody>
-        <?php
+            </thead>
+            <tbody>
+            <?php
             foreach($cliente->getListaDeClientes() as $obj) {
                 echo "<tr>";
                 echo "<td>" . $obj->getNome() . "</td>";
@@ -33,8 +63,19 @@ $cliente = new ClienteController();
                 echo "<td>" . $obj->getEndereco() . "</td>";
                 echo "</tr>";
             }
-        ?>
-        </tbody>
-    </table>
+            ?>
+            </tbody>
+        </table>
+
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">Exercicio PHP</p>
+        </div>
+    </footer>
+
+    <script type='text/javascript' src='assets/js/jquery.js'></script>
+    <script type='text/javascript' src='assets/js/bootstrap.js'></script>
 </body>
 </html>
