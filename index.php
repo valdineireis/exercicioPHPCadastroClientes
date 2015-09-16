d7e8e193aa228d7deabcd12b8aff7d0f8b0f2b6d
Valdinei Reis <valdineimtz@gmail.com>
2015-09-16 00:35:11 -0300

    Página de listagem dos Clientes

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: valdinei
- * Date: 16/09/15
- * Time: 00:15
- */
+
+require_once "Cliente.php";
+require_once "ClienteController.php";
+
+$cliente = new ClienteController();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Cadastro de Clientes</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Exercício básico de PHP demonstrando um simples cadastro de clientes em memória">
+    <meta name="author" content="Valdinei Reis">
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>CPF</th>
+                <th>Ednereço</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            foreach($cliente->getListaDeClientes() as $obj) {
+                echo "<tr>";
+                echo "<td>" . $obj->getNome() . "</td>";
+                echo "<td>" . $obj->getCpf() . "</td>";
+                echo "<td>" . $obj->getEndereco() . "</td>";
+                echo "</tr>";
+            }
+        ?>
+        </tbody>
+    </table>
+</body>
+</html>
